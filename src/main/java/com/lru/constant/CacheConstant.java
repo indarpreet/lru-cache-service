baf57b2c879a83aece2a7a5abba9ba7c2354aa25
isokhi <isokhi@ISOKHI-Y1BA3.cisco.com>
2019-04-24 02:53:02 -0700

    added componenet scan action url

--- src/main/java/com/lru/constant/CacheConstant.java
+++ src/main/java/com/lru/constant/CacheConstant.java
@@ -5,5 +5,6 @@ public class CacheConstant {
 	public static final String GET= "/get/{id}";
 	public static final String PUT= "/put/{id}";
 	public static final String MANAGE_CACHE_ACTION = "api/v1";
+	public static final String COMPONENT_SCAN_ACTION = "com.lru";
 
 }
--- src/main/java/com/lru/controller/SpringBootWebApplication.java
+++ src/main/java/com/lru/controller/SpringBootWebApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.lru.constant.CacheConstant;
 import com.lru.model.LRUCache;
 
 @SpringBootApplication
-@ComponentScan(value="com.lru")
+@ComponentScan(value=CacheConstant.COMPONENT_SCAN_ACTION)
 public class SpringBootWebApplication {
 
 	public static void main(String[] args) {
